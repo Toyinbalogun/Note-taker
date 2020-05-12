c9e3575f124b58624fab8a5e55f803b96f68ebb0
Toyinbalogun <balogunt302@gmail.com>
2020-05-12 14:50:00 -0400

    connected all API route calls

--- server.js
+++ server.js
@@ -1,270 +1,99 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+//Dependencies
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
 
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = process.env.PORT || 3000;
+//express config
+const app = express()
+
+//set up port
+const PORT = process.env.PORT || 3000
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(__dirname + "/public"))
 
-// Random ID generator for notes
-// =============================================================
-
-const generateId = () => {
-  let characters = "1234567890qwertyuiopasdfghjklzxcvbnm";
-  let id = "";
-  for (let i = 0; i < 8; i++) {
-    let index = Math.floor(Math.random() * characters.length);
-    id += characters[index];
-  }
-  return id;
-};
-
-// Routes
-// =============================================================
-
-// Note page route
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-// API routes
-app.get("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    res.json(JSON.parse(data));
-  });
-});
-
-app.get("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    let notes = JSON.parse(data);
-    for (const note of notes) {
-      if (note.id == id) {
-        console.log(note);
-        return res.json(note);
-      }
+function generateID(){
+    let char = "1234567890qwertyuiopasdfghjklzxcvbnm"
+    let id = ""
+    for (let i = 0; i < 8; i++) {
+      let index = Math.floor(Math.random() * char.length)
+      id += char[index]
     }
-    return res.json(false);
-  });
-});
+    return id
+}
 
-app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  newNote.id = generateId();
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    let notes = JSON.parse(data);
-    notes.push(newNote);
-    fs.writeFile(
-      path.join(__dirname, "db/db.json"),
-      JSON.stringify(notes),
-      "utf8",
-      (err) => {
-        if (err) throw err;
-        console.log("The file has been saved with the new note.");
-        res.send(newNote);
-      }
-    );
-  });
-});
+//Router
+//Note page route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + "public/notes.html"))
+})
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-    let notes = JSON.parse(data);
-    for (const note of notes) {
-      if (note.id == id) {
-        notes.splice(notes.indexOf(note), 1);
+//API routes
+app.get('/api/notes', (req, res)=> {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/api/notes/:id', function(req, res){
+    const id = req.params.id
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
+       let notes = JSON.parse(data)
+
+       for (const note of notes){
+           if (note.id == id){
+               console.log(note)
+               return res.json(note)
+           }
+       }
+       return res.json(false)
+    })
+})
+
+app.post('/api/notes', (req, res)=> {
+    let nNote = req.body
+    nNote.id = generateID()
+
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
+        let notes = JSON.parse(data)
+        note.push(nNote)
         fs.writeFile(
-          path.join(__dirname, "db/db.json"),
-          JSON.stringify(notes),
-          "utf8",
-          (err) => {
-            if (err) throw err;
-            console.log("The file has been saved with the requested deletion.");
-          }
-        );
-
-        return res.send(true);
-      }
-    }
-    return res.json(false);
-  });
-});
-
-// Default page route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Dependencies
-// const express = require('express')
-// const path = require('path')
-// const fs = require('fs')
-
-// //express config
-// const app = express()
-
-// //set up port
-// const PORT = process.env.PORT || 3000
-
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
-// app.use(express.static(__dirname + "/public"))
-
-// function generateID(){
-//     let char = "1234567890qwertyuiopasdfghjklzxcvbnm"
-//     let id = ""
-//     for (let i = 0; i < 8; i++) {
-//       let index = Math.floor(Math.random() * char.length)
-//       id += char[index]
-//     }
-//     return id
-// }
-
-// //Router
-// //Note page route
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname + "public/notes.html"))
-// })
-
-// //API routes
-// app.get('/api/notes', (req, res)=> {
-//     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
-//         res.json(JSON.parse(data))
-//     })
-// })
-
-// app.get('/api/notes/:id', function(req, res){
-//     const id = req.params.id
-//     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
-//        let notes = JSON.parse(data)
-
-//        for (const note of notes){
-//            if (note.id == id){
-//                console.log(note)
-//                return res.json(note)
-//            }
-//        }
-//        return res.json(false)
-//     })
-// })
-
-// app.post('/api/notes', (req, res)=> {
-//     let nNote = req.body
-//     nNote.id = generateID()
-
-//     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
-//         let notes = JSON.parse(data)
-//         note.push(nNote)
-//         fs.writeFile(
-//             path.join(__dirname, "db/db.json"),
-//             JSON.stringify(notes), "utf8", (err)=> {
-//                 if (err) throw err
-//                 console.log("New Note Saved")
-//                 res.send(nNote)
-//             }
-//         )
-//     })  
-// })
-
-// app.delete('/api/notes/:id', (req, res)=>{
-//     const id = req.params.id
-//     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
-//        let notes = JSON.parse(data)
-
-//        for (const note of notes){
-//            if (note.id == id){
-//               notes.splice(notes.indexOf(note), 1)
-//               fs.writeFile(
-//                 path.join(__dirname, "db/db.json"),
-//                 JSON.stringify(notes), "utf8", (err)=> {
-//                     if (err) throw err
-//                     console.log("Note Deleted")
-//                 }
-//               )
-//               return res.send(true)
-//            }
-//        }
-//        return res.json(false)
-//     })
-// })
-
-// app.get("*", (req, res) =>{
-//     res.sendFile(path.join(__dirname, "public/index.html"))
-// })
-// //Listener
-// app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
+            path.join(__dirname, "db/db.json"),
+            JSON.stringify(notes), "utf8", (err)=> {
+                if (err) throw err
+                console.log("New Note Saved")
+                res.send(nNote)
+            }
+        )
+    })  
+})
+
+app.delete('/api/notes/:id', (req, res)=>{
+    const id = req.params.id
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) =>{
+       let notes = JSON.parse(data)
+
+       for (const note of notes){
+           if (note.id == id){
+              notes.splice(notes.indexOf(note), 1)
+              fs.writeFile(
+                path.join(__dirname, "db/db.json"),
+                JSON.stringify(notes), "utf8", (err)=> {
+                    if (err) throw err
+                    console.log("Note Deleted")
+                }
+              )
+              return res.send(true)
+           }
+       }
+       return res.json(false)
+    })
+})
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+//Listener
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`))
